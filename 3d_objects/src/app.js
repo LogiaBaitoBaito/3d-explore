78d3bf623fa9ada15e4c8a66fcc0748ccfdb9ee1
jzuriaga <juazuri@gmail.com>
2016-12-22 23:44:22 +0100

    First scene

--- 3d_objects/src/app.js
+++ 3d_objects/src/app.js
@@ -4,60 +4,69 @@
  * Tutorial: https://code.tutsplus.com/tutorials/webgl-with-threejs-basics--net-35688
  */
 
-import { WebGLRenderer, Scene, CubeGeometry, MeshBasicMaterial, Clock, BackSide,
-  PointLight, MeshLambertMaterial, Mesh, PerspectiveCamera } from 'three';
+import * as THREE from 'three';
 
   'use strict';
 
+window.onload = () => {
 
   var width = window.innerWidth;
   var height = window.innerHeight;
 
-  var renderer = new WebGLRenderer({ antialias: true });
+  var scene = new THREE.Scene;
+  var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
+
+  var renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(width, height);
+  renderer.setClearColor(0xEEEEEE, 1.0);
 
-  var scene = new Scene;
+  var axes = new THREE.AxisHelper(20);
+  scene.add(axes);
 
+  var planeGeometry = new THREE.PlaneGeometry(60, 20, 1, 1);
+  var planeMaterial = new THREE.MeshBasicMaterial({color: 0xCCCCCC});
+  var plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
-  var cubeGeometry = new CubeGeometry(100, 100, 100);
-  var cubeMaterial = new MeshLambertMaterial({ color: 0x1ec876 });
-  var cube = new Mesh(cubeGeometry, cubeMaterial);
+  plane.rotation.x = -0.5 * Math.PI;
+  plane.position.x = 15;
+  plane.position.y = 0;
+  plane.position.z = 0;
+  scene.add(plane);
 
-  cube.rotation.y = Math.PI * 45 / 180;
+  var cubeGeometry = new THREE.CubeGeometry(4, 4, 4);
+  var cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+  var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+  cube.position.x = -4;
+  cube.position.y = 3;
+  cube.position.z = 0; 
 
   scene.add(cube);
 
-  var camera = new PerspectiveCamera(45, width / height, 0.1, 10000);
+  var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+  var sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x7777ff, wireframe:true });
+  var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 
-  camera.position.y = 160;
-  camera.position.z = 400;
+  sphere.position.x = 20;
+  sphere.position.y = 4;
+  sphere.position.z = 2;
+
+  scene.add(sphere);
+
+  var render = () => {
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  };
+  render();
+
+  camera.position.x = -30;
+  camera.position.y = 40;
+  camera.position.z = 30;
+  camera.lookAt(scene.position);
 
   scene.add(camera);
 
-  var clock = new Clock;
-
-  function render() {
-    renderer.render(scene, camera);
-
-    cube.rotation.y -= clock.getDelta();
-    requestAnimationFrame(render);
-  }
-  render();
-
-
-  camera.lookAt(cube.position);
-
-  var skyboxGeometry = new CubeGeometry(10000, 10000, 10000);
-  var skyboxMaterial = new MeshBasicMaterial({ color: 0x000000, side: BackSide });
-  var skybox = new Mesh(skyboxGeometry, skyboxMaterial);
-
-  scene.add(skybox);
-
-  var pointLight = new PointLight(0xffffff);
-  pointLight.position.set(0, 300, 200);
-
-  scene.add(pointLight);
-
-
   document.body.appendChild(renderer.domElement);
 
+};
