42c7b246e8eb09cef92853d91394574d00cce81b
jzuriaga <juazuri@gmail.com>
2016-12-30 13:22:49 +0100

    Add control with limits

--- 3js_scene/src/__test/control.spec.js
+++ 3js_scene/src/__test/control.spec.js
@@ -1,6 +1,6 @@
 import {expect} from 'chai';
 import Control from '../control';
-import dat from '../../node_modules/dat.gui/build/dat.gui.min.js';
+import dat from 'dat.gui/build/dat.gui.min';
 
 describe('Control', () => {
 
@@ -15,6 +15,8 @@ describe('Control', () => {
     expect(control.init).to.be.a('function');
     expect(control.controls).to.be.a('object');
     expect(control.controls).to.be.empty;
+    expect(control.cconfig).to.be.a('object');
+    expect(control.cconfig).to.be.empty;
   });
 
   it('should add controls', () => {
@@ -22,6 +24,23 @@ describe('Control', () => {
     control.add('dummyControl', 1);
 
     expect(control.controls).to.deep.equal({dummyControl: 1});
+    expect(control.cconfig).to.be.empty;
+  });
+
+  it('should add with limits', () => {
+    control = new Control();
+    control.addWithLimits('dummyControl', 1, 1, 2);
+
+    expect(control.controls).to.deep.equal({dummyControl: 1});
+    expect(control.cconfig).to.deep.equal({dummyControl:{lower:1,higher:2}});
+  });
+
+  it('should add with default limits', () => {
+    control = new Control();
+    control.addWithLimits('dummyControl', 2);
+
+    expect(control.controls).to.deep.equal({dummyControl: 2});
+    expect(control.cconfig).to.deep.equal({dummyControl:{lower:0,higher:2}});
   });
 
   it('should get controls', () => {
@@ -32,7 +51,6 @@ describe('Control', () => {
 
     expect(control.controls).to.deep.equal({dummyControl: 1});
     expect(result).to.be.equal(1);
-
   });
 
   it('should init controls', () => {
--- 3js_scene/src/control.js
+++ 3js_scene/src/control.js
@@ -4,13 +4,23 @@ export default class Control {
 
   constructor() {
     this.add = this.add.bind(this);
+    this.addWithLimits = this.addWithLimits.bind(this);
     this.init = this.init.bind(this);
 
     this.controls = {};
+    this.cconfig = {};
   }
 
   add (control, value) {
     this.controls[control] = value;
+  }
+
+  addWithLimits(control, value, lower = 0, higher) {
+    this.add(control, value);
+    this.cconfig[control] = {
+      lower: lower,
+      higher: higher || value
+    }
   }
 
   get (control) {
@@ -21,7 +31,11 @@ export default class Control {
     this.gui = new dat.GUI();
 
     for (let control in this.controls) {
-      this.gui.add(this.controls, control, 0, 0.5);
+      if (this.cconfig[control] && this.cconfig[control].lower ) {
+        this.gui.add(this.controls, control, lower, higher);
+      } else {
+        this.gui.add(this.controls, control);
+      }
     };
   }
 
