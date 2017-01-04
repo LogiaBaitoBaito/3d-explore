3ff4ebccf189c9acc1b0b6ed5ffc98a5026397b4
juazugas <juazuri@gmail.com>
2017-01-04 18:07:06 +0100

    Configure webpack to use a multiple page

--- 3js_scene/webpack.config.js
+++ 3js_scene/webpack.config.js
@@ -1,14 +1,33 @@
 var path = require('path');
+var CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 
 module.exports = {
-  entry: [
-    './src/app.js'
-  ],
+  entry: {
+    vendor: ['three', 'dat.gui/build/dat.gui.min', 'stats.js'],
+    // commons: ['./src/control.js', './src/stats.js'],
+    bundle: './src/app.js'
+  },
   output: {
     path: __dirname,
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
+  plugins: [
+    new CommonsChunkPlugin({
+      name: "vendor",
+      filename: "vendor.js",
+      minChunks: Infinity
+    }),
+    new CommonsChunkPlugin({
+      name: "commons",
+      filename: "commons.js",
+      minChunks: 1,
+      chunks: ['bunde']
+
+      // chunks: ["pageA", "pageB"],
+      // (Only use these entries)
+    })
+  ],
   module: {
     preLoaders: [
       {test: /\.js$/, loader: "eslint-loader", exclude: /node_modules/}
