93285c32df9d5b25cd2b1d9f7155d2534389f726
juazugas <juazuri@gmail.com>
2017-01-08 23:51:49 +0100

    Play with geometry properties

--- 3js_scene/src/component/custom-geometry.js
+++ 3js_scene/src/component/custom-geometry.js
@@ -8,52 +8,101 @@ export default class CustomGeometry  extends BaseScene {
   }
 
   addControls() {
-    this.control.add('outputObjects', () => {
-      console.log(this._scene.children);
+
+    let addPosition = (x, y, z) => {
+      return { x, y, z };
+    }
+    let controlPoints = [];
+    controlPoints.push(addPosition(3, 5, 3));
+    controlPoints.push(addPosition(3, 5, 0));
+    controlPoints.push(addPosition(3, 0, 3));
+    controlPoints.push(addPosition(3, 0, 0));
+    controlPoints.push(addPosition(0, 5, 0));
+    controlPoints.push(addPosition(0, 5, 3));
+    controlPoints.push(addPosition(0, 0, 0));
+    controlPoints.push(addPosition(0, 0, 3));
+
+    controlPoints.map( (point, i) => {
+      this.control.addWithLimits(`Vertice ${i} x`, point.x, -10, 10);
+      this.control.addWithLimits(`Vertice ${i} y`, point.y, -10, 10);
+      this.control.addWithLimits(`Vertice ${i} z`, point.z, -10, 10);
     });
-    this.control.add('camera-x', -50);
-    this.control.add('camera-y', 30);
-    this.control.add('camera-z', 20);
-    this.control.addWithListen('numberOfObjects', this._scene.children.length);
   }
 
   drawGeometry () {
-    let geoms = [];
 
-    // Cube (b , w, h)
-    geoms.push(new THREE.BoxGeometry(2, 2, 2));
+    let vertices = [
+      new THREE.Vector3(1, 3, 1),
+      new THREE.Vector3(1, 3, -1),
+      new THREE.Vector3(1, -1, 1),
+      new THREE.Vector3(1, -1, -1),
+      new THREE.Vector3(-1, 3, -1),
+      new THREE.Vector3(-1, 3, 1),
+      new THREE.Vector3(-1, -1, -1),
+      new THREE.Vector3(-1, -1, 1)
+    ];
+
+    let faces = [
+      new THREE.Face3(0, 2, 1),
+      new THREE.Face3(2, 3, 1),
+      new THREE.Face3(4, 6, 5),
+      new THREE.Face3(6, 7, 5),
+      new THREE.Face3(4, 5, 1),
+      new THREE.Face3(5, 0, 1),
+      new THREE.Face3(7, 6, 2),
+      new THREE.Face3(6, 3, 2),
+      new THREE.Face3(5, 7, 0),
+      new THREE.Face3(7, 2, 0),
+      new THREE.Face3(1, 3, 4),
+      new THREE.Face3(3, 6, 4),
+    ];
+
+    let geom = new THREE.Geometry();
+    geom.vertices = vertices;
+    geom.faces = faces;
+    geom.computeFaceNormals();
+
+    let materials = [
+      new THREE.MeshLambertMaterial({
+        opacity:0.6,
+        color: 0x44ff44,
+        transparent: true}),
+      new THREE.MeshBasicMaterial({
+        wireframe: true,
+        color: 0x000000})
+    ];
 
     // draw in scene
-    let col = 0;
-    geoms.map( (geom, index) => {
-      let materials = [
-        new THREE.MeshLambertMaterial({color: Math.random() * 0xffffff, shading: THREE.FlatShading }),
-        new THREE.MeshBasicMaterial({wireframe: true, color: 0x000000})
-      ];
+    let mesh = THREE.SceneUtils.createMultiMaterialObject(geom, materials);
+    mesh.traverse( (element) => element.castShadow = true );
 
-      let mesh = THREE.SceneUtils.createMultiMaterialObject(geom, materials);
-      mesh.traverse( (element) => element.castShadow = true );
+    this._scene.add(mesh);
 
-      mesh.position.x = -24 + ((index % 4) * 12);
-      mesh.position.y = 4;
-      mesh.position.z = -8 + (col * 12);
+    return geom;
+ }
 
-      if ((index+1) % 4 === 0) col++;
-      this._scene.add(mesh);
-    });
+  positionScene () {
+    this._plane.rotation.x = -0.5 * Math.PI;
+    this._plane.position.x = 0;
+    this._plane.position.y = 0;
+    this._plane.position.z = 0;
 
-    return geoms;
+    this._camera.position.x = -20;
+    this._camera.position.y = 25;
+    this._camera.position.z = 20;
+    this._camera.lookAt(new THREE.Vector3(5, 0, 0));
   }
 
   draw() {
     super.draw();
+    this.positionScene();
     this.drawGeometry();
   }
 
   renderControls() {
-    this._camera.position.x = this.control.get('camera-x');
-    this._camera.position.y = this.control.get('camera-y');
-    this._camera.position.z = this.control.get('camera-z');
+    //this._camera.position.x = this.control.get('camera-x');
+    //this._camera.position.y = this.control.get('camera-y');
+    //this._camera.position.z = this.control.get('camera-z');
   }
 
 }
